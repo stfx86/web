71a1715df9473ce6839a2488d146e63805cd8033
stfx <mustaphabennasser8@gmail.com>
2025-05-08 14:27:45 +0100

    tp3 done2

--- tp3/modifier.php
+++ tp3/modifier.php
@@ -1,5 +1,6 @@
 <?php
 // Enable error reporting at the top of your script
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
@@ -8,88 +9,113 @@ require "connexion.php";
 
 // globals
 $formaData = [
-  "naissance" => '',
-  "nom" => '',
-  "prenom" => '',
-  "naissance" => '',
-  "mat" => '',
-  "sex" => '',
-  "adresse" => ''
+    "naissance" => '',
+    "nom" => '',
+    "prenom" => '',
+    "mat" => '',
+    "sex" => '',
+    "adresse" => ''
 ];
 $error = '';
 
 
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  global $formaData, $error;
+
+//GET
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+
+    $mat = isset($_GET['mat']) ? (int)$_GET['mat'] : 0;
+    $query = "select * from bd_tp_php.etudiants where mat={$mat}";
+    $resp = mysqli_query($bd, $query);
+    if (mysqli_num_rows($resp) == 0) {
+        $error = "no one with the matricule $mat";
+        header("location: modifier.php?error=$error");
+        exit(11);
+    }
+
+
+    $row = mysqli_fetch_assoc($resp);
+    // echo "from get___________________";
+    // print_r($row)."<br><br>";
+    // echo "___________________";
+    foreach ($row as $k => $v) {
+        global $formaData;
+        $formaData["$k"] = $row["$k"];
+    }
+    // echo "from get_22__________________";
+    // print_r($formaData)."<br><br>";
+    // echo "___22________________";
+
+
+}
 
 
 
 
-  foreach ($formaData as $k => $v) {
 
-    if (isset($_POST[$k]) && !empty($_POST[$k])) {
-      $formaData[$k] = htmlspecialchars($_POST[$k]);
+//POSt
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
+    // echo $_POST['submit'];
+    global $formaData, $error;
+
+
+
+
+    foreach ($formaData as $k => $v) {
+
+        if (isset($_POST[$k]) && !empty($_POST[$k])) {
+            $formaData[$k] = htmlspecialchars($_POST[$k]);
+        } else {
+            $error = "you must specify $k";
+            header("location: modifier.php?error=$error");
+            break;
+        }
+    }
+    $formaData['mat'] = (int)$formaData['mat'];
+    //
+    // echo "testing formaData ";
+    // foreach ($formaData as $k => $v) {
+    //   echo "$k" . "====>" . "$v" . "<br><br>";
+    // }
+    //
+
+
+
+
+    //checkig if matricul not  existing 
+
+    $mat = $formaData['mat'];
+
+    // $error = "Matricule must be a number not \"$mat\" ";
+    // header("location: ajout_form.php?error=$error");
+
+
+    $query = "select mat   from bd_tp_php.etudiants where mat=$mat";
+    $delete_query  = "delete  from  bd_tp_php.etudiants where mat=$mat";
+    $insert_query = "insert into  bd_tp_php.etudiants (mat, nom, prenom, naissance, sex, adresse)
+    VALUES ( 
+     $mat ,
+    '{$formaData['nom']}' ,
+    '{$formaData['prenom']}' ,
+    '{$formaData['naissance']}' ,
+    '{$formaData['sex']}' ,
+    '{$formaData['adresse']}'
+      )";
+
+
+
+
+    $resp = mysqli_query($bd, $query);
+    if (mysqli_num_rows($resp) != 1) {
+        $error = "matriccl do not  exist ";
+        header("location: modifier.php?error=$error");
     } else {
-      $error = "you must specify $k";
-      header("location: ajout_form.php?error=$error");
-      break;
+        mysqli_query($bd, $delete_query);
+        mysqli_query($bd, $insert_query);
+        header("location: liste_etudiants.php");
     }
-  }
-
-  //
-  // echo "testing formaData ";
-  // foreach ($formaData as $k => $v) {
-  //   echo "$k" . "====>" . "$v" . "<br><br>";
-  // }
-  //
-
-
-  //connect to the dataabse 
-  // $bd = mysqli_connect('localhost', 'stof', 'bennasser', 'bd_tp_php') or die("connection faild");
-
-  //checkig if matricul already exist 
-
-  $mat = $formaData['mat'];
-  if(!is_int($mat)){
-    $error = "Matricule must be a number not \"$mat\" ";
-    header("location: ajout_form.php?error=$error");
-    exit(1);
-
-  }
-  $query = "select mat , nom , prenom  from bd_tp_php.etudiants where mat=$mat";
-
-
-
-
-  $resp = mysqli_query($bd, $query);
-  if (mysqli_num_rows($resp) != 0) {
-    $error = "matriccl already exist choose a diffrent one";
-    header("location: ajout_form.php?error=$error");
-  } else {
-
-    $query = "insert into  bd_tp_php.etudiants (mat, nom, prenom, naissance, sex, adresse)
-     VALUES ( 
-      {$formaData['mat']} ,
-     '{$formaData['nom']}' ,
-     '{$formaData['prenom']}' ,
-     '{$formaData['naissance']}' ,
-     '{$formaData['sex']}' ,
-     '{$formaData['adresse']}'
-       )";
-    echo $query . "<br><br>";
-    if (mysqli_query($bd, $query)) {
-
-      header("location: liste_etudiants.php");
-    }
-  }
 }
- if($_SERVER['REQUEST_METHOD']==='GET'){
 
-
-
-
-}
 
 
 
@@ -107,91 +133,99 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <title>Document</title>
-  <style>
-    label {
-      display: inline-block;
-      width: 150px;
-      vertical-align: top;
-    }
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <style>
+        label {
+            display: inline-block;
+            width: 150px;
+            vertical-align: top;
+        }
 
-    div {
+        div {
 
-      margin: 6px;
-    }
+            margin: 6px;
+        }
 
-    .loisir {
-      display: inline-flex;
+        .loisir {
+            display: inline-flex;
 
-    }
+        }
 
-    fieldset {
-      display: inline-table;
+        fieldset {
+            display: inline-table;
 
-    }
-  </style>
+        }
+    </style>
 </head>
 
 <body>
+    <?php
+    print_r($formaData);
+
+
+    ?>
     <h1>Modifier Etudiants</h1>
-  <fieldset>
-    <form method="post" action="<?= $_SERVER['PHP_SELF'] ?>">
+    <fieldset>
+        <form method="post" action="<?= $_SERVER['PHP_SELF'] ?>">
 
 
-      <div> <label> Matricule </label>
-        <input type="text" name="mat" required>
-      </div>
+            <div> <label> Matricule </label>
+                <input type="text" name="mat" value="<?= $formaData["mat"] ?>">
+            </div>
 
 
-      <div> <label> nom</label>
-        <input type="text" name="nom" required>
-      </div>
-      <div>
-        <label>prenom </label>
-        <input type="text" name="prenom" required>
-      </div>
+            <div> <label> nom</label>
+                <input type="text" name="nom" required value="<?= $formaData["nom"] ?>">
+            </div>
+            <div>
+                <label>prenom </label>
+                <input type="text" name="prenom" value="<?= $formaData["prenom"] ?>" required>
+            </div>
 
 
-      <div>
-        <label>Date de Naissance </label>
-        <input type="date" name="naissance" />
-      </div>
+            <div>
+                <label>Date de Naissance </label>
+                <input type="date" name="naissance" value="<?= $formaData["naissance"] ?>" />
+            </div>
 
-      <div>
+            <div>
 
-        <label>Sexe</label>
-        <select name="sex">
+                <label>Sexe</label>
+                <select name="sex">
 
-          <option value="feminin">Femme</option>
-          <option value="masculin">Homme</option>
-        </select>
-      </div>
+                    <option value="feminin" <?php if ($formaData["sex"] == 'feminin')
 
-      <div>
-        <label>Adresse</label>
-        <input type="text" name="adresse" required>
-      </div>
 
-      <div>
-        <button type="submit" value="modifier">Modifier</button>
-        <button type="reset"> effacer</button>
+                                                echo "selected";  ?>>Femme</option>
+                    <option value="masculin" <?php if ($formaData["sex"] == 'masculin')
 
-      </div>
 
-    </form>
-  </fieldset>
+                                                    echo "selected";  ?>>Homme</option>
+                </select>
+            </div>
+
+            <div>
+                <label>Adresse</label>
+                <input type="text" name="adresse" value="<?= $formaData["adresse"] ?>" required>
+            </div>
+
+            <div>
+                <button type="submit" name="submit" value="modifier">Modifier</button>
+                <button type="reset"> effacer</button>
+
+            </div>
+
+        </form>
+    </fieldset>
 
 </body>
 
 <?php
 
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-
-  if (isset($_GET['error'])) {
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['error'])) {
 
     echo "<script>alert('" . $_GET['error'] . "')</script>";
-  }
 }
 
 ?>
